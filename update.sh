7ab964ca21803e02f6e06dbc47f0ce8a62d4f26c
DaddyWarcrimes <4444646+DaddyWarcrimes@users.noreply.github.com>
2021-12-10 22:25:32 -0600

    Automated backup of dotfiles on 20211210_222532

--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@ for FILE in $(find ~/dotfiles -type f -name ".*"); do
 #	rm $FILE
 	cp ~/$(basename $FILE) $FILE
 done
-now=`date '+%Y%m%d_%h%m%S'`
+now=`date '+%Y%m%d_%H%M%S'`
 git commit -am "Automated backup of dotfiles on $now"
 git push

cc03089969e801e2ede90753ccffb5d090ddf4e9
DaddyWarcrimes <4444646+DaddyWarcrimes@users.noreply.github.com>
2022-09-20 15:46:56 -0500

    Modified update script to hopefully run from anywhere. This commit is my test of the new functionality

--- update.sh
+++ update.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
+cd ~/dotfiles
 echo "Commit message:"
 read M
 for FILE in $(find ~/dotfiles -type f -name ".*"); do
-#	rm $FILE
 	cat ~/$(basename $FILE) > $FILE
 done
-#now=`date '+%Y%m%d_%H%M%S'`
 git commit -am "$M"
 git push

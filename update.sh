61a9dbf68a173d6e6cea9efc8d0799092ee3ad6a
stephenp-al <4444646+StephenP-AL@users.noreply.github.com>
2024-10-20 19:51:23 -0500

    Better dotfile selection

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $SCRIPT_DIR
 echo "Commit message:"
 read M
-for FILE in $(find $SCRIPT_DIR -type f -name ".*" ); do
+for FILE in $(git ls-tree $(git rev-parse --abbrev-ref HEAD) --name-only | grep '^\.'); do
 	cat ~/$(basename $FILE) > $FILE
 done
 git commit -am "$M"
